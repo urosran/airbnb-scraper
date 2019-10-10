ff44308a49be5973d906892cfe26769843480117
Uros Randelovic <uros.randelovic@gmail.com>
2019-10-10 17:58:36 +0200

    bash script fixed

--- belgrade.sh
+++ belgrade.sh
@@ -4,6 +4,12 @@
 
 # loop over prices from 0 to 100 in increments of 2
 for price_max in {2..100..2}; do
-   price_min = $price_max - 2
-   node link_parsing.js -price_min $price_min -price_max $price_max -city "belgrade" -checkin "2020-01-20" -checkout "2020-01-26"
+   PRICE_MAX=2
+   # this is how you do math in bash else it does nothing
+   PRICE_MIN=$(expr $PRICE_MAX - 2)
+   # pring what price it's currently at
+   echo price max $PRICE_MAX
+   echo price min $PRICE_MIN
+   # execute a command
+   node link_parsing.js --price_min $PRICE_MIN --price_max $PRICE_MAX --city "belgrade" --checkin "2020-01-20" --checkout "2020-01-26"
 done
