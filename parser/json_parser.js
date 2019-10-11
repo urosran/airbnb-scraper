5ce7909c05184da8cef9553fccceb0ec3de6268c
Uros Randelovic <uros.randelovic@gmail.com>
2019-10-11 13:42:10 +0200

    parser extracts the listings and saves it to results folder

--- parser/json_parser.js
+++ parser/json_parser.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+let listingIds = []
+let city = undefined
 // get all JSON files from a single folder
 var _getAllFilesFromFolder = function (dir) {
     var results = [];
@@ -17,13 +19,26 @@ var _getAllFilesFromFolder = function (dir) {
     return results;
 };
 
+function writeFileJSON(file, extension = ".json", city="some_city") {
+    console.log(file)
+    let fileName = '../results/listingIds_' + city + extension
+    fs.writeFile(fileName, JSON.stringify(file), function (err) {
+        if (err) throw err;
+        console.log('Saved to: ' + fileName);
+    })
+}
 //return all listing IDs from locally stored JSONs
 function extractListingIds(pathToJSONfile) {
     let rawdata = fs.readFileSync(pathToJSONfile);
     let json = JSON.parse(rawdata);
-    console.log(json);
+    console.log(json.bootstrapData.reduxData.exploreTab.response.explore_tabs[0].home_tab_metadata.remarketing_ids)
+    console.log(listingIds)
+    listingIds = listingIds.concat(json.bootstrapData.reduxData.exploreTab.response.explore_tabs[0].home_tab_metadata.remarketing_ids)
+    console.log(listingIds)
+    city = json.bootstrapData.reduxData.exploreTab.response.metadata.query
 }
 
 //Loop trough each JSON in a given folder and extract AirBnB listing IDs
 let files = _getAllFilesFromFolder("C:\\Users\\urosr\\Apps\\storage\\beograd")
 files.forEach(extractListingIds)
+writeFileJSON(file=listingIds, city=this.city)
